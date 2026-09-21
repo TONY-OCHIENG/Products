d02fe1659a3b1441c089c1c6de73c9690ed6df5e
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-09-21 17:09:11 +0300

    Setting up express server

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,12 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+dotenv.config()
 
 const app = express()
-app.listen(3000, () => {
+app.use(express.json())
+app.use(cors())
+
+app.listen(process.env.PORT, () => {
     console.log("Server is running")
 })
