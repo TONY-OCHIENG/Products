246e6f737b5227dda4d5678b43353e955565ceaa
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-09-21 17:32:25 +0300

    Adding product endpoint

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import productRoutes from './routes/product.route'
 dotenv.config()
 
 const app = express()
 app.use(express.json())
 app.use(cors())
+app.use("/api/products",productRoutes)
 
 app.listen(process.env.PORT, () => {
     console.log("Server is running")
---
+++ Backend/routes/product.route.js
@@ -0,0 +1,6 @@
+import express from 'express'
+import { addProducts } from '../controllers/product.controller'
+
+const productRoutes = express.Router()
+productRoutes.post("addProducts",addProducts)
+export default productRoutes
