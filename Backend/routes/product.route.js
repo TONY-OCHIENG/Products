1616984f4f7f5c5e6a492d97196b90a54693fa0a
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-09-22 10:11:17 +0300

    Adding delete endpoint

--- Backend/routes/product.route.js
+++ Backend/routes/product.route.js
@@ -1,10 +1,10 @@
 import express from 'express'
-import { addProducts, getAllProducts, updateProduct } from '../controllers/product.controller.js'
+import { addProducts, deleteProduct, getAllProducts, updateProduct } from '../controllers/product.controller.js'
 import { upload } from '../configs/upload.js'
 
 const productRoutes = express.Router()
 productRoutes.post("/addProducts",upload('image'), addProducts)
 productRoutes.get("/allProducts",getAllProducts)
 productRoutes.put("/updateProduct/:id",updateProduct)
-
+productRoutes.delete("/deleteProduct/:id",deleteProduct)
 export default productRoutes
