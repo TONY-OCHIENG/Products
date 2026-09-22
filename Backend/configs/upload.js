3884ce28849b28ccb5dbaf666cb56d11450cf670
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-09-22 09:38:06 +0300

    Adding upload middleware

--- Backend/configs/upload.js
+++ Backend/configs/upload.js
@@ -3,7 +3,7 @@ import path from 'path'
 
 const storage = multer.diskStorage({
     destination: (req,file,cb) => {
-        cb(null,'server/public/images')
+        cb(null,'Backend/public/images')
     },
     filename: (req,file,cb) => {
         cb(null,file.filename + "_" + Date.now() + path.extname(file.originalname))
--- Backend/routes/product.route.js
+++ Backend/routes/product.route.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { addProducts } from '../controllers/product.controller.js'
+import { upload } from '../configs/upload.js'
 
 const productRoutes = express.Router()
-productRoutes.post("/addProducts",addProducts)
+productRoutes.post("/addProducts",upload('image'), addProducts)
 export default productRoutes
