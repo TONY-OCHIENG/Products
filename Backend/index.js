98688a04284202970322e9d36daa7276922a9ce6
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-09-22 09:40:08 +0300

    Enablic the public folder to be accessed on the frontend

--- Backend/index.js
+++ Backend/index.js
@@ -7,6 +7,7 @@ dotenv.config()
 const app = express()
 app.use(express.json())
 app.use(cors())
+app.use(express.static('Backend/public'))
 app.use("/api/products",productRoutes)
 
 app.listen(process.env.PORT, () => {
