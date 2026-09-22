bb6b06e10c1e3be4e48cc82082321d93d22b9929
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-09-22 10:10:34 +0300

    Adding update endpoint

--- Backend/routes/product.route.js
+++ Backend/routes/product.route.js
@@ -1,8 +1,10 @@
 import express from 'express'
-import { addProducts, getAllProducts } from '../controllers/product.controller.js'
+import { addProducts, getAllProducts, updateProduct } from '../controllers/product.controller.js'
 import { upload } from '../configs/upload.js'
 
 const productRoutes = express.Router()
 productRoutes.post("/addProducts",upload('image'), addProducts)
 productRoutes.get("/allProducts",getAllProducts)
+productRoutes.put("/updateProduct/:id",updateProduct)
+
 export default productRoutes
