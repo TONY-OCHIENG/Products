91f6dfd595a32520c88608923730b6c6bb543e40
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-09-24 18:30:11 +0300

    Adding a single endpoint

--- Backend/routes/product.route.js
+++ Backend/routes/product.route.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { addProducts, deleteProduct, getAllProducts, updateProduct } from '../controllers/product.controller.js'
+import { addProducts, deleteProduct, getAllProducts, singleProduct, updateProduct } from '../controllers/product.controller.js'
 import { upload } from '../configs/upload.js'
 
 const productRoutes = express.Router()
@@ -7,4 +7,5 @@ productRoutes.post("/addProducts",upload.single('image'), addProducts)
 productRoutes.get("/allProducts",getAllProducts)
 productRoutes.put("/updateProduct/:id",updateProduct)
 productRoutes.delete("/deleteProduct/:id",deleteProduct) 
+productRoutes.get("/singleProduct/:id",singleProduct)
 export default productRoutes
